1c15de50529f8c38d4e320349c807b2fa3c67142
Tomasz Plonka <t0biash-iodb@wp.pl>
2011-07-05 17:09:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iodb::Application.config.secret_token = 'ad0538688df467b5cc95c61dccadaa58d8973e2b8a46dc90314275937b1f92b3ad670b8b4edc6ae042eaaa47bd64aa940556f3a52ec650bfe30c9089eb755c18'
