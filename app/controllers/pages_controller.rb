9cc168eba6dfab1a6ad511558dfafa7674843765
t0biash <t0biash-iodb@wp.pl>
2011-08-23 15:45:54 +0200

    Proper info on about, home, contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+	  @title = "Strona Główna"
   end
 
   def contact
+	  @title = "Kontakt"
   end
 
   def about
+	  @title = "O stronie"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,25 +1,29 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
   describe "GET 'home'" do
-    it "should be successful" do
+    it "should have right title" do
       get 'home'
-      response.should be_success
+      response.should have_selector("title",
+				    :content => "IODB | Strona Główna")
     end
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    it "should have right title" do
       get 'contact'
-      response.should be_success
+      response.should have_selector("title",
+				    :content => "IODB | Kontakt")
     end
   end
 
   describe "GET 'about'" do
-    it "should be successful" do
+    it "should have right title" do
       get 'about'
-      response.should be_success
+      response.should have_selector("title",
+				    :content => "IODB | O stronie")
     end
   end
 
